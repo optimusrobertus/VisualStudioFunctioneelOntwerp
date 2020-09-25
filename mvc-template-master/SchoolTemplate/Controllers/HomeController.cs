b08553913c5b7b9fbd9b893eb8d91e39f9a61b63
optimusrobertus <robertdilber11@gmail.com>
2020-09-25 09:04:16 +0200

    cleaned up top nav and footer; fixed last page paths

--- mvc-template-master/SchoolTemplate/Controllers/HomeController.cs
+++ mvc-template-master/SchoolTemplate/Controllers/HomeController.cs
@@ -8,10 +8,39 @@ using SchoolTemplate.Models;
 
 namespace SchoolTemplate.Controllers
 {
+<<<<<<< HEAD
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
     string connectionString = "Server=172.16.160.21;Port=3306;Database=110041;Uid=110041;Pwd=MEdenkgR;";
+=======
+    public class HomeController : Controller
+    {
+        // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
+        string connectionString = "Server=172.16.160.21;Port=3306;Database=110041;Uid=110041;Pwd=MEdenkgR;";
+
+        // [Route("Index")]
+        public IActionResult Index()
+        {
+            List<Festival> festivals = new List<Festival>();
+            // uncomment deze regel om producten uit je database toe te voegen
+            festivals = GetFestivals();
+
+            return View(festivals);
+        }
+
+        [Route("account")]
+        public IActionResult account()
+        {
+            return View();
+        }
+
+        [Route("contact")]
+        public IActionResult contact()
+        {
+            return View();
+        }
+>>>>>>> cleaned up top nav and footer; fixed last page paths
 
     public IActionResult Index()
     {
@@ -38,6 +67,7 @@ namespace SchoolTemplate.Controllers
             int Id = Convert.ToInt32(reader["Id"]);
             string Naam = reader["Naam"].ToString();
                         string Img = reader["Img"].ToString();
+<<<<<<< HEAD
 
 
 
@@ -49,6 +79,18 @@ namespace SchoolTemplate.Controllers
                             Beschrijving = reader["beschrijving"].ToString(),
                             datum = DateTime.Parse(reader["datum"].ToString()),
                             Img = reader["Img"].ToString(),
+=======
+            
+
+            Festival p = new Festival
+            {
+              Id = Id,
+              Naam = Naam,
+              Beschrijving = reader["beschrijving"].ToString(),
+              datum = DateTime.Parse(reader["datum"].ToString()),
+              Img = reader["Img"].ToString(),
+              
+>>>>>>> cleaned up top nav and footer; fixed last page paths
             };
             products.Add(p);
           }
