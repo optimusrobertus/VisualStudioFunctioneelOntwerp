975cf9cb8982cc9dad1afc03982c88b1fbd29dd9
optimusrobertus <robertdilber11@gmail.com>
2020-09-25 11:49:56 +0200

    Contact form added

--- mvc-template-master/SchoolTemplate/Controllers/HomeController.cs
+++ mvc-template-master/SchoolTemplate/Controllers/HomeController.cs
@@ -8,12 +8,6 @@ using SchoolTemplate.Models;
 
 namespace SchoolTemplate.Controllers
 {
-<<<<<<< HEAD
-  public class HomeController : Controller
-  {
-    // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=110041;Uid=110041;Pwd=MEdenkgR;";
-=======
     public class HomeController : Controller
     {
         // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
@@ -40,16 +34,14 @@ namespace SchoolTemplate.Controllers
         {
             return View();
         }
->>>>>>> cleaned up top nav and footer; fixed last page paths
 
-    public IActionResult Index()
-    {
-      List<Festival> festivals = new List<Festival>();
-        // uncomment deze regel om producten uit je database toe te voegen
-        festivals = GetFestivals();
-
-        return View(festivals);
-    }
+    
+        [Route("contact")]
+        [HttpPost]
+        public IActionResult contact(PersonModel model)
+        {
+            return View();
+        }
 
     private List<Festival> GetFestivals()
     {
@@ -66,22 +58,7 @@ namespace SchoolTemplate.Controllers
           {
             int Id = Convert.ToInt32(reader["Id"]);
             string Naam = reader["Naam"].ToString();
-                        string Img = reader["Img"].ToString();
-<<<<<<< HEAD
-
-
-
-
-                        Festival p = new Festival
-                        {
-                            Id = Id,
-                            Naam = Naam,
-                            Beschrijving = reader["beschrijving"].ToString(),
-                            datum = DateTime.Parse(reader["datum"].ToString()),
-                            Img = reader["Img"].ToString(),
-=======
-            
-
+            string Img = reader["Img"].ToString();
             Festival p = new Festival
             {
               Id = Id,
@@ -89,8 +66,6 @@ namespace SchoolTemplate.Controllers
               Beschrijving = reader["beschrijving"].ToString(),
               datum = DateTime.Parse(reader["datum"].ToString()),
               Img = reader["Img"].ToString(),
-              
->>>>>>> cleaned up top nav and footer; fixed last page paths
             };
             products.Add(p);
           }
---
+++ mvc-template-master/SchoolTemplate/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolTemplate.Models
+{
+    public class PersonModel
+    {
+        [Required(ErrorMessage = "First name is mandatory")] 
+        public string firstname { get; set; }
+
+        [Required(ErrorMessage ="Last name is mandatory")]
+        public string lastname { get; set; }
+
+        [Required(ErrorMessage ="E-mail address is mandatory")]
+        [EmailAddress(ErrorMessage ="Not a valid e-mail address")]
+        public string email { get; set; }
+
+        public DateTime birthdate { get; set; }
+    }
+
+}
