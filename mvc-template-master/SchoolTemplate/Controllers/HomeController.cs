b46badf64eac0bdd10d32e624a5b7f347b0be439
Ramez Karius <110041@st-maartenscollege.nl>
2020-10-14 21:59:53 +0200

    thuis werkend krijgen, moet commiten?

--- mvc-template-master/SchoolTemplate/Controllers/HomeController.cs
+++ mvc-template-master/SchoolTemplate/Controllers/HomeController.cs
@@ -1,75 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient;
-using SchoolTemplate.Database;
-using SchoolTemplate.Models;
+﻿#using System;
+#using System.Collections.Generic;
+#using System.Diagnostics;
+#using Microsoft.AspNetCore.Mvc;
+#using MySql.Data.MySqlClient;
+#using SchoolTemplate.Database;
+#using SchoolTemplate.Models;
 
-namespace SchoolTemplate.Controllers
-{
-    public class HomeController : Controller
+# namespace SchoolTemplate.Controllers
+#{
+  #  public class HomeController : Controller
     {
         // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-        string connectionString = "Server=172.16.160.21;Port=3306;Database=110041;Uid=110041;Pwd=MEdenkgR;";
+    #    string connectionString = "Server=172.16.160.21;Port=3306;Database=110041;Uid=110041;Pwd=MEdenkgR;";
 
-        public IActionResult Index()
+    #    public IActionResult Index()
         {
-            List<Festival> festivals = new List<Festival>();
+      #      List<Festival> festivals = new List<Festival>();
             // uncomment deze regel om producten uit je database toe te voegen
-            festivals = GetFestivals();
+      #      festivals = GetFestivals();
 
-            return View(festivals);
+       #     return View(festivals);
         }
 
-        [Route("account")]
-        public IActionResult account()
+    #    [Route("account")]
+    #    public IActionResult account()
         {
-            return View();
+    #        return View();
         }
 
-        [Route("contact")]
-        public IActionResult contact()
+  #      [Route("contact")]
+   #     public IActionResult contact()
         {
-            return View();
+    #        return View();
         }
 
-        [Route("FAQ")]
-        public IActionResult FAQ()
+     #   [Route("FAQ")]
+#    public IActionResult FAQ()
         {
-            return View();
+     #       return View();
         }
 
-        [Route("field")]
-        public IActionResult field()
+     #   [Route("field")]
+     #   public IActionResult field()
         {
-            return View();
+     #       return View();
         }
 
-        [Route("line")]
-        public IActionResult line()
+      #  [Route("line")]
+       # public IActionResult line()
         {
-            return View();
+      #      return View();
         }
 
-    private List<Festival> GetFestivals()
+   ## private List<Festival> GetFestivals()
     {
-      List<Festival> products = new List<Festival>();
+    ##  List<Festival> products = new List<Festival>();
 
-      using (MySqlConnection conn = new MySqlConnection(connectionString))
+   ##   using (MySqlConnection conn = new MySqlConnection(connectionString))
       {
         conn.Open();
-        MySqlCommand cmd = new MySqlCommand("select * from festival", conn);
+      ##  MySqlCommand cmd = new MySqlCommand("select * from festival", conn);
 
-        using (var reader = cmd.ExecuteReader())
+       ## using (var reader = cmd.ExecuteReader())
         {
-          while (reader.Read())
+       ##   while (reader.Read())
           {
-            int Id = Convert.ToInt32(reader["Id"]);
-            string Naam = reader["Naam"].ToString();
+# int Id = Convert.ToInt32(reader["Id"]);
+           # string Naam = reader["Naam"].ToString();
             
 
-            Festival p = new Festival
+        #    Festival p = new Festival
             {
               Id = Id,
               Naam = Naam,
@@ -82,14 +82,14 @@ namespace SchoolTemplate.Controllers
         }
       }
 
-      return products;
+    #  return products;
     }
 
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+  #  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  #  public IActionResult Error()
     {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  #    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
   }
 }
