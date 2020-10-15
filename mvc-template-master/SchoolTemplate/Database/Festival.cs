cbde68dda1643f3049f1abfa0a82aca4b66af8f2
optimusrobertus <robertdilber11@gmail.com>
2020-10-15 19:55:38 +0200

    New upcoming page

--- mvc-template-master/SchoolTemplate/Database/Festival.cs
+++ mvc-template-master/SchoolTemplate/Database/Festival.cs
@@ -2,17 +2,19 @@
 
 namespace SchoolTemplate.Database
 {
-  public class Festivals
+  public class Festival
   {
     public int Id { get; set; }
     
     public string Naam { get; set; }
 
-    public string Beschrijving{ get; set; }   
-
-        public string Img { get; set; }
+    public string Beschrijving{ get; set; }
+        
+    public string Img { get; set; }
         
     public DateTime Datum { get; set; }
+
+    public Decimal Prijs { get; set; }
 
     /// <summary>
     /// Gebruik altijd decimal voor geldzaken. Dit doe je om te voorkomen dat er afrondingsfouten optreden
